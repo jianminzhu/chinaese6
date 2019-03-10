649148ac7eee7441874b99db31827bff7031dd07
zjm <zhujianmin2009@163.com>
2019-03-10 14:51:54 +0800

    pp sdk

---
+++ application/app/start.php
@@ -0,0 +1,12 @@
+<?php
+require "autoload.php";
+define('SITE_URL', 'http://travelling.chinesecompanion.com');
+//创建支付对象实例
+$tocke = "An5ns1Kso7MWUdW4ErQKJJJ4qi4-AkCYw9L-9wCJGRakRli0E8DrQpWb";
+
+$paypal = new \PayPal\Rest\ApiContext(
+    new \PayPal\Auth\OAuthTokenCredential(
+        'realmarketplace-facilitator@yahoo.com',
+        $tocke
+    )
+);
