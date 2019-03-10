132bb45319b3340927939f11beebba13de3055f5
zjm <zhujianmin2009@163.com>
2019-03-10 15:00:21 +0800

    add spider

--- application/app/start.php
+++ application/app/start.php
@@ -1,5 +1,5 @@
 <?php
-require "autoload.php";
+require "PayPal-PHP-SDK/autoload.php";
 define('SITE_URL', 'http://travelling.chinesecompanion.com');
 //创建支付对象实例
 $tocke = "An5ns1Kso7MWUdW4ErQKJJJ4qi4-AkCYw9L-9wCJGRakRli0E8DrQpWb";
