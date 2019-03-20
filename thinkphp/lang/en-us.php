33fb09bcdae89da4cf1c0f7fc40bff0755f39655
zjm <zhujianmin2009@163.com>
2019-03-20 14:35:41 +0800

    profile add concats

--- thinkphp/lang/en-us.php
+++ thinkphp/lang/en-us.php
@@ -31,6 +31,8 @@ return [
     "邮箱" => "email",
     "微信" => "WeChat",
     "城市" => "city",
+    "省" => "State/Province",
+
     "会员动态" => "Activity",
     "谁在找我" => "Interested In Me",
     "加我为最爱" => "I'm Their Favorite",
