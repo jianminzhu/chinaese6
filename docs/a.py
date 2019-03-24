d75f9cc967ba8ffbd352d7a21868e077a77c2db5
zjm <zhujianmin2009@163.com>
2019-03-25 04:32:39 +0800

    python run

--- docs/a.py
+++ docs/a.py
@@ -1,15 +1,22 @@
 # !/usr/bin/env python
 # -*- coding: utf-8 -*-
-import time
-import urllib.request
+import urllib2,time
 
 
-def spider(url):
+def getHtml(url):
+    header = {"User-Agent":"Mozilla/5.0 (Macintosh; Intel Mac OS X 10.12; rv:48.0) Gecko/20100101 Firefox/48.0"}
+    request = urllib2.Request(url=url,headers=header) #模拟浏览器进行访问
+    response = urllib2.urlopen(request)
+    text = response.read()
+    return text
+def spider():
 
     while True:
-        fp = urllib.request.urlopen("http://travelling.chinesecompanion.com/index.php/index/spby/pics?isShowPic=no&limit=1")
-        mybytes = fp.read()
-        print(mybytes)
+        html = getHtml("http://travelling.chinesecompanion.com/index.php/index/spby/pics?isShowPic=no&limit=1")
+        print(html)
         print ("Start : %s" % time.ctime())
         time.sleep( 5 )
         print ("End : %s" % time.ctime())
+
+if __name__ == '__main__':
+    spider()
