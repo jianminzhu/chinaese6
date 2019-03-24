c48198eacaed68dd4003f883d67c76e79ae9484c
zjm <zhujianmin2009@163.com>
2019-03-25 04:42:40 +0800

    python run

--- docs/a.py
+++ docs/a.py
@@ -1,22 +1,34 @@
 # !/usr/bin/env python
 # -*- coding: utf-8 -*-
-import urllib2,time
+import threading
+import time
+import urllib2
 
 
 def getHtml(url):
-    header = {"User-Agent":"Mozilla/5.0 (Macintosh; Intel Mac OS X 10.12; rv:48.0) Gecko/20100101 Firefox/48.0"}
-    request = urllib2.Request(url=url,headers=header) #模拟浏览器进行访问
+    header = {"User-Agent": "Mozilla/5.0 (Macintosh; Intel Mac OS X 10.12; rv:48.0) Gecko/20100101 Firefox/48.0"}
+    request = urllib2.Request(url=url, headers=header)  # 模拟浏览器进行访问
     response = urllib2.urlopen(request)
     text = response.read()
     return text
-def spider():
 
+
+def spider():
     while True:
-        html = getHtml("http://travelling.chinesecompanion.com/index.php/index/spby/pics?isShowPic=no&limit=1")
+        html = getHtml("http://travelling.chinesecompanion.com/index.php/index/spby/pics?isShowPic=no&limit=100")
         print(html)
         print ("Start : %s" % time.ctime())
-        time.sleep( 5 )
+        time.sleep(1)
         print ("End : %s" % time.ctime())
 
+
 if __name__ == '__main__':
-    spider()
+    t = []
+    for index in range(10):
+        t.append(threading.Thread(target=spider))
+
+    for index in range(len(t)):
+        t[index].start()
+
+    for index in range(len(t)):
+        t[index].join()
