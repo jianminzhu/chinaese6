5d8f623f0b06c9aa03526218111fb08c520e466e
zjm <zhujianmin2009@163.com>
2019-03-25 04:56:45 +0800

    python run

--- docs/a.py
+++ docs/a.py
@@ -3,7 +3,14 @@
 import threading
 import time
 import urllib2
+# SELECT                "bmember"  as type, COUNT(*) AS bmember FROM bmember WHERE isDownPics=0
+# UNION ALL SELECT      "memberby "  as type, COUNT(*) AS memberby  FROM memberby
+# UNION ALL SELECT      "membercontact"  as type, COUNT(*) AS membercontact FROM membercontact
+# UNION ALL SELECT      "memberlevel"  as type, COUNT(*) AS memberlevel FROM memberlevel
+# UNION ALL SELECT      "member "  as type, COUNT(*) AS member  FROM member
 
+# cd ~/tt
+# nohup python a.py> output10.html 2>&1 &
 
 def getHtml(url):
     header = {"User-Agent": "Mozilla/5.0 (Macintosh; Intel Mac OS X 10.12; rv:48.0) Gecko/20100101 Firefox/48.0"}
