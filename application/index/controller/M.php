d26da6214d1c8450f4e9207a12572ad43a1fcc07
zjm <zhujianmin2009@163.com>
2019-03-26 23:04:02 +0800

    python run

--- application/index/controller/M.php
+++ application/index/controller/M.php
@@ -214,9 +214,12 @@ class M extends Base
             $emsg = $e->getMessage();
         }
         if ($isPay) {
-            $vip = Db::query(" SELECT *, TIMESTAMPDIFF(DAY,startdate,enddate)AS remaining FROM pay where m_id=$id")[0];
-            $vip["renge"] = $vip["cost"]>199?lang("终身"):$vip["startdate"] . " ~ " . $vip["enddate"];
-            $vip["remainingDays"] = $vip["cost"]>199?lang("终身"): $vip['remaining'] ." 天";
+            try {
+                $vip = Db::query(" SELECT *, TIMESTAMPDIFF(DAY,startdate,enddate)AS remaining FROM pay where m_id=$id")[0];
+                $vip["renge"] = $vip["cost"] > 199 ? lang("终身") : $vip["startdate"] . " ~ " . $vip["enddate"];
+                $vip["remainingDays"] = $vip["cost"] > 199 ? lang("终身") : $vip['remaining'] . " 天";
+            } catch (\Exception $e) {
+            }
         }
 
         return ['m' => $member, "pics" => $pics, "cc" => $cc, "emsg" => $emsg,"isPay"=>$isPay,"vip"=> $vip];
