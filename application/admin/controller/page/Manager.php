b0bbd80f6e7080f75ea71326273119a119b61f66
zjm <zhujianmin2009@163.com>
2019-03-30 20:29:31 +0800

    python run

--- application/admin/controller/page/Manager.php
+++ application/admin/controller/page/Manager.php
@@ -25,5 +25,33 @@ class Manager extends Backend
         return view("page/memberList");
     }
 
+    public function delpics(){
+        $mid = request()->param("mid");
+        $picid = request()->param("picid");
+        if ($mid &&$mid != "undefined") {
+            $m = Db::table("member")->where("id", $mid)->find() ;
+            if ($m) {
+                try {
+                    Db::table("del_pics")->insert(["mid" => $mid, "file_path" => $m["main_pic"], "type" => "main", "picid" => 0]);
+                    Db::table("member")->where("id", $mid)->update(["main_pic"=> "/nv_toux_del.jpg"]);
+                } catch (\Exception $e) {
+                }
+            }
+        }
+        if($picid && $picid != "undefined") {
+            try {
+                $pic = Db::table("pics")->where("id", $picid)->find();
+                if ($pic) {
+                    Db::table("del_pics")->insert(["file_path" => $pic["file_path"],"picid"=>$picid, "type" => "pics", "mid" => $pic["m_id"]]);
+                    Db::table("pics")->where("id", $picid)->delete();
+                }
+            } catch (\Exception $e) {
+                echo $e->getMessage();
+            }
+        }
+        return json(["isSuccess" => 0]);
+
+    }
+
 
 }
