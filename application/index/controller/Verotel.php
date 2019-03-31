d09d3397f1fea23cfe36811d9bdaedf46b28f851
zjm <zhujianmin2009@163.com>
2019-04-01 07:49:57 +0800

    python run

--- application/index/controller/Verotel.php
+++ application/index/controller/Verotel.php
@@ -32,19 +32,36 @@ class Verotel extends Base
     {
 
         try {
-            $param = request()->param();
-            if (validate_signature($param)) {
-                try {
-                    db("verotel_pay")->insert($param);
-                } catch (\Exception $e) {
-                    echo $e->getMessage();
-                }
-            }
+            $this->updateUserPayStatus();
             $this->headData();
             return "支付成功";//view("/index/paysucc",["data"=>json_encode($param)]);
         } catch (\Exception $e) {
         }
         return "从新支付";
+    }
+    public function updateUserPayStatus(){
+        try {
+            if ($this->isLogin()) {
+                $mid = $this->loginUser()->id;
+                $items= db("verotel_user")->where("mid",$mid)->where("is_pay_succ",0)->select();
+                foreach ($items as $item) {
+                    try {
+                        $params=json_decode(ExtGetHtml($items["url"]));
+                        if (validate_signature($params)) {
+                            $saleID = $params["saleID"];
+                            db("verotel_user")->where("mid", $mid)->where("is_pay_succ", 0)->update(["saleID" => $saleID, "is_pay_succ" => "1"]);
+                            db("pay")->insert(["m_id"=>$mid,"type"=>1,"cost"=>$params["priceAmount"]]);
+                            echo "succ ";
+                        }
+                    } catch (\Exception $e) {
+                    }
+                }
+            }
+
+        } catch (\Exception $e) {
+            echo $e->getMessage();
+        }
+
     }
 
     public function cancel()
