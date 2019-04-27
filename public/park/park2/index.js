684fbc77fb68d60fd030e795c4455a81788f1cd7
zjm <zhujianmin2009@163.com>
2019-04-27 17:33:18 +0800

    park2 video

--- public/park/park2/index.js
+++ public/park/park2/index.js
@@ -1,4 +1,9 @@
 $(function () {
+    function wait(ms) {
+        return new Promise((resolve) => {
+            setTimeout(resolve, ms)
+        })
+    }
     let srcW = 3427;
     let srcH = 1920;
     let width = 1920;
@@ -14,6 +19,12 @@ $(function () {
         font: "38px Digital-7Mono",
         fillColor: "#42A6B3",
         text: moment().format('YYYY/MM/DD   HH:mm:ss')
+    }
+    cars = {
+        car1: {id:"car1",pos: [851, 353], textures:"images/cars/violet/quan_violet_01.png"  },
+        car2: {id:"car2",pos: [851, 403], textures:"images/cars/blue/quan_blue_01.png"  },
+        car3: {id:"car3",pos: [850, 323], textures:"images/cars/pink/quan_01.png"  },
+        car4: {id:"car4",pos: [850, 303], textures:"images/cars/green/quan_01.png"  },
     }
 
     histogram = {
@@ -33,7 +44,9 @@ $(function () {
             , {color: "blue", date: "4/15", value: 530}
             , {color: "blue", date: "4/16", value: 98}
             , {color: "pink", date: "4/17", value: 200}
-        ]
+        ],
+        total: "" + (367 + 590 + 322 + 419 + 530 + 98 + 200),
+        lastWeekTotal: "2658"
     }
     linesArr = {
         "1": {img: "images/lines/line1.png", siteNum: 9, name: "1号线"},
@@ -50,11 +63,46 @@ $(function () {
         "ss": {name: "森森", img: "images/persons/ss.png"}
     }
     runCars = {
-        "B100801": { cardNo: "B100801", personId: "ls", lineNo: "4", speed: " 32", upDate: "2019/04/26", upTime: "18:35:00"},
-        "B100802": { cardNo: "B100802", personId: "xb", lineNo: "3", speed: "  0", upDate: "2019/04/26", upTime: "18:35:00"},
-        "B100808": { cardNo: "B100808", personId: "xz", lineNo: "5", speed: " 26", upDate: "2019/04/26", upTime: "18:35:00"},
-        "B100809": { cardNo: "B100809", personId: "ss", lineNo: "6", speed: " 60", upDate: "2019/04/26", upTime: "18:35:00"},
-        "B100812": { cardNo: "B100812", personId: "ls", lineNo: "1", speed: " 16", upDate: "2019/04/26", upTime: "18:35:00"}
+        "B100801": {
+            cardNo: "B100801",
+            personId: "ls",
+            lineNo: "4",
+            speed: " 32",
+            upDate: "2019/04/26",
+            upTime: "18:35:00"
+        },
+        "B100802": {
+            cardNo: "B100802",
+            personId: "xb",
+            lineNo: "3",
+            speed: "  0",
+            upDate: "2019/04/26",
+            upTime: "18:35:00"
+        },
+        "B100808": {
+            cardNo: "B100808",
+            personId: "xz",
+            lineNo: "5",
+            speed: " 26",
+            upDate: "2019/04/26",
+            upTime: "18:35:00"
+        },
+        "B100809": {
+            cardNo: "B100809",
+            personId: "ss",
+            lineNo: "6",
+            speed: " 60",
+            upDate: "2019/04/26",
+            upTime: "18:35:00"
+        },
+        "B100812": {
+            cardNo: "B100812",
+            personId: "ls",
+            lineNo: "1",
+            speed: " 16",
+            upDate: "2019/04/26",
+            upTime: "18:35:00"
+        }
     }
     logisticsConf = {
         name: {font: "18px Adobe Heiti Std R", fillColor: "#7E95C6"},
@@ -66,11 +114,22 @@ $(function () {
         {per: 20, name: "生活用品", num: 10028},
         {per: 40, name: "饮料　", num: 16998},
     ]
-    alarms={
-        today:{num:" 6"}
-
+    alarms = {
+        today: {num: " 6"}
     }
-
+    weather = {
+        pos: [79, 631],
+        temperature: {pos: [123, 90], text: "16", font: '30px bold MantekaCyrillic-Regular', fillColor: "#FFF"},
+        temperatureC: {pos: [160, 90], text: "℃", font: '30px  ArialMT', fillColor: "#AAC2EC"},
+        type: {pos: [123, 160], text: "大雨", font: '30px bold MantekaCyrillic-Regular', fillColor: "#FFF"},
+        typeImg: {pos: [48, 145], textures: "images/weather/weather_bigRain.png"},
+        windDirect: {pos: [135, 235], font: '12px "Adobe Heiti Std R"', fillColor: "#AAC2EC", text: "风向:"},
+        windDirectText: {pos: [135, 255], font: '18px "Adobe Heiti Std R"', fillColor: "#FFF", text: "东南"},
+        windPower: {pos: [185, 235], font: '12px "Adobe Heiti Std R"', fillColor: "#AAC2EC", text: "风力:"},
+        windPowerText: {pos: [185, 255], font: '18px "Adobe Heiti Std R"', fillColor: "#FFF", text: "9级"},
+        warning: {pos: [55, 325], textures: "images/weather/台风预警.png"},
+        warningText: {pos: [45, 375], text: "台风预警", font: '16px "MicrosoftYaHeiUI"', fillColor: "#AAC2EC"},
+    }
     data = {
         resolution: resolution,
         viewport: [srcW / (srcW / width), srcH / (srcW / width)],
@@ -91,19 +150,7 @@ $(function () {
         logistics,
         alarms,
         date: today,
-        weather: {
-            pos: [79, 631],
-            temperature: {pos: [123, 90], text: "16", font: '30px bold MantekaCyrillic-Regular', fillColor: "#FFF"},
-            temperatureC: {pos: [160, 90], text: "℃", font: '30px  ArialMT', fillColor: "#AAC2EC"},
-            type: {pos: [123, 160], text: "大雨", font: '30px bold MantekaCyrillic-Regular', fillColor: "#FFF"},
-            typeImg: {pos: [55, 145], textures: "images/weather/weather_bigRain.png"},
-            windDirect: {pos: [135, 235], font: '12px "Adobe Heiti Std R"', fillColor: "#AAC2EC", text: "风向:"},
-            windDirectText: {pos: [135, 255], font: '18px "Adobe Heiti Std R"', fillColor: "#FFF", text: "东南"},
-            windPower: {pos: [185, 235], font: '12px "Adobe Heiti Std R"', fillColor: "#AAC2EC", text: "风力:"},
-            windPowerText: {pos: [185, 255], font: '18px "Adobe Heiti Std R"', fillColor: "#FFF", text: "9级"},
-            warning: {pos: [55, 325], textures: "images/weather/台风预警.png"},
-            warningText: {pos: [45, 375], text: "台风预警", font: '16px "MicrosoftYaHeiUI"', fillColor: "#AAC2EC"},
-        },
+        weather,
 
         lines: [
             {
@@ -157,25 +204,33 @@ $(function () {
     })
     setInterval(function () {
         try {
+            weather["temperature"]["text"] = 10 + Math.floor(28 * Math.random())
+            let num = Math.floor(68 * Math.random());
+            alarms.today.num = num < 10 ? " " + num : num;
+            let total = 0;
             for (const his of histogram["values"]) {
-                his["value"] = Math.floor(Number(histogram["yArr"][0] )* Math.random())
+                let kNumber = Math.floor(Number(histogram["yArr"][0]) * Math.random());
+                total += kNumber
+                his["value"] = kNumber
                 console.log(his["value"])
             }
+            histogram.total = total + ""
+
         } catch (e) {
         }
-    },1000)
+    }, 1000)
     setInterval(function () {
         try {
             for (const his of logistics) {
-              let per= Math.floor(100* Math.random())
+                let per = Math.floor(100 * Math.random())
                 if (per > 90) {
-                    per=90
+                    per = 90
                 }
-                his["per"]=per
+                his["per"] = per
             }
         } catch (e) {
         }
-    },1000)
+    }, 1000)
 
     setInterval(function () {
         today["text"] = moment().format('YYYY/MM/DD   HH:mm:ss')
@@ -203,4 +258,36 @@ $(function () {
             }, Math.ceil(5000 * Math.random()))
         }
     }, 2000)
+    // setInterval(function () {
+    //         var a=m.$refs.rcars[0]
+    //         let car = cars["car1"];
+    //         var index=car["index"]||1
+    //         if (index > 100) {
+    //             index = 0;
+    //         }
+    //         var pos= a.attr().pos
+    //         a.attr({pos: [++pos[0],++pos[1]],textures : `0${index++%10}.png`})
+    //
+    //     console.log( car["textures"]);
+    // }, 300)
+
+
+
+    // a=m.$refs.rcars[0]
+    // rangeGen=function(start, end, fun) {
+    //     return Array(end - start + 1).fill(0).map((v, i) => fun(i + start))
+    // }
+    // a.animate(rangeGen(1, 12, function (i) {
+    //     var color = "violet";
+    //     return {"textures":`images/cars/${color}/quan_${color}_${i<10?"0":""}${i}.png`};
+    // }, {
+    //     duration: 500,
+    //     direction: 'alternate',
+    //     iterations: Infinity,
+    // }));
+    cars.car1.pos=[cars.car1.pos[0]+10,cars.car1.pos[1]+10]
+
+
+
+
 })
