c91eb9eb661188fb701c10b3484db2138f29da95
zjm <zhujianmin2009@163.com>
2019-04-30 10:19:22 +0800

    p1

--- public/park/extjs/PardAdv.ts
+++ public/park/extjs/PardAdv.ts
@@ -13,15 +13,34 @@ class PardAdv{
     getCar(id) {
         return this.cars[id];
     }
-    addCar(id,x=0,y=0,pic='./static/img/car.png') {
+    
+     genCarImgs(img) {
+        let prefix = img.replace(/\d{1,2}\.png$/, "")
+         var arr = [];
+         for (var i = 1; i <= 12; i++) {
+             arr.push({textures: `${prefix}${i + 1}.png`})
+         }
+         return arr;
+    };
+    
+    addCar(id,x=0,y=0,pic='./park2/images/cars/pink/1.png') {
         const { Sprite} = spritejs
         const car = new Sprite(pic)
         car.attr({
             anchor: [0, 0.3],
+            size: [60, 40],
             pos: [x,y],
             borderRadius: 50,
         })
         this.cars[id] = car;
+        v=this;
+        setTimeout(function () {
+            car.animate(v.genCarImgs(pic), {
+                duration: 1000,
+                iterations: Infinity,
+                direction: 'alternate',
+            });
+        },1000 * Math.random());
         this.layer.append(car);
         return car;
     }
